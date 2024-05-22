d7e34ef832e4dea7f4ba1351e9052c7c8e6207e0
rfcfa-iscte <78759743+rfcfa-iscte@users.noreply.github.com>
2024-05-22 11:15:18 +0100

    Add ResponseEntity and error message on movieController

--- backend/src/main/java/com/example/MovieEnthusiast/controller/MovieController.java
+++ backend/src/main/java/com/example/MovieEnthusiast/controller/MovieController.java
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.example.MovieEnthusiast.services.MovieService;
 import com.example.MovieEnthusiast.model.Movie;
@@ -27,24 +26,39 @@ public class MovieController {
     }
 
     @GetMapping
-    public List<SimpleMovie> getAllMovies(@RequestParam(defaultValue = "1") int page) {
-        return movieService.fetchSimpleMovies(page);
+    public ResponseEntity<?> getAllMovies(@RequestParam(defaultValue = "1") int page) {
+        List<SimpleMovie> movieList = movieService.fetchSimpleMovies(page);
+        if (movieList.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Could not find movies at page " + page);
+        }
+        return ResponseEntity.ok(movieList);
     }
 
     @GetMapping("/movieDetails/{id}")
-    public ResponseEntity<Movie> getMovieDetails(@PathVariable long id) {
+    public ResponseEntity<?> getMovieDetails(@PathVariable long id) {
         Movie movieDetails = movieService.fetchMovieDetails(id);
+        if (movieDetails == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Movie details not found");
+        }
         return ResponseEntity.ok(movieDetails);
     }
 
     @GetMapping("/topRevenue")
-    public List<SimpleMovie> getTopRevenueMovies() {
-        return movieService.fetchTopRevenueMovies();
+    public ResponseEntity<?> getTopRevenueMovies() {
+        List<SimpleMovie> topRevenueMovies = movieService.fetchTopRevenueMovies();
+        if (topRevenueMovies.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Top revenue movies not found");
+        }
+        return ResponseEntity.ok(topRevenueMovies);
     }
 
     @GetMapping("/topRevenue/{year}")
-    public ResponseEntity<List<SimpleMovie>> getTopRevenueMoviesPerYear(@PathVariable long year) {
+    public ResponseEntity<?> getTopRevenueMoviesPerYear(@PathVariable long year) {
         List<SimpleMovie> movieDetails = movieService.fetchTopRevenueMoviesPerYear(year);
+        if (movieDetails == null || movieDetails.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Top revenue movies of the year " + year + " not found");
+        }
         return ResponseEntity.ok(movieDetails);
     }
 
